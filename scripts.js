755a6aa8ed7406217ec2a73767ad08937382f2b7
alineelias <149534479+alineelias@users.noreply.github.com>
2023-11-16 19:15:48 -0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,18 @@
+var listaFilmes = 
+    ["https://br.web.img3.acsta.net/medias/nmedia/18/91/90/98/20169244.jpg", "https://static.wixstatic.com/media/da06f2_9e0748f8e4fc45eba701ee2bd5581c11~mv2.jpg/v1/fill/w_640,h_998,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/da06f2_9e0748f8e4fc45eba701ee2bd5581c11~mv2.jpg", "https://musicart.xboxlive.com/7/94fc5000-0000-0000-0000-000000000002/504/image.jpg?w=1920&h=1080"]; 
+
+// listaFilmes[0] =
+//  'https://br.web.img3.acsta.net/medias/nmedia/18/91/90/98/20169244.jpg'
+// listaFilmes[1] = 'https://static.wixstatic.com/media/da06f2_9e0748f8e4fc45eba701ee2bd5581c11~mv2.jpg/v1/fill/w_640,h_998,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/da06f2_9e0748f8e4fc45eba701ee2bd5581c11~mv2.jpg'
+// listaFilmes[2] = 'https://musicart.xboxlive.com/7/94fc5000-0000-0000-0000-000000000002/504/image.jpg?w=1920&h=1080'
+
+
+for(var i=0; i< listaFilmes.length; i++) {
+  document.write('<img src=' + listaFilmes[i] +'>')
+}
+
+
+
+
+  
+
